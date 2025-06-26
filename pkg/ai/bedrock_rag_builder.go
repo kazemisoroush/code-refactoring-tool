ade7c7645e06ab97935d12084a8a34f5baa50361
Soroush Kazemi <skazemi@Soroushs-MacBook-Pro-2.local>
2025-06-26 21:09:24 +1000

    Adds s3 and kb sdk.

--- pkg/ai/bedrock_rag_builder.go
+++ pkg/ai/bedrock_rag_builder.go
@@ -4,15 +4,24 @@ package ai
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/bedrockagent"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/kazemisoroush/code-refactoring-tool/pkg/repository"
 )
 
 // BedrockRAGBuilder is an implementation of RAGBuilder that uses AWS Bedrock for building the RAG pipeline.
-type BedrockRAGBuilder struct{}
+type BedrockRAGBuilder struct {
+	s3Client *s3.Client
+	kbClient *bedrockagent.Client
+}
 
 // NewBedrockRAGBuilder creates a new instance of BedrockRAGBuilder.
-func NewBedrockRAGBuilder() (RAGBuilder, error) {
-	return BedrockRAGBuilder{}, nil
+func NewBedrockRAGBuilder(cfg aws.Config) (RAGBuilder, error) {
+	return &BedrockRAGBuilder{
+		s3Client: s3.NewFromConfig(cfg),
+		kbClient: bedrockagent.NewFromConfig(cfg),
+	}, nil
 }
 
 // Build implements RAGBuilder.
